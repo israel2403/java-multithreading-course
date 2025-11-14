2ecf59d46b8abce37222ccbe86273be3272712eb
Israel Huerta Fer <israelhf24@gmail.com>
2025-11-13 22:25:25 -0600

    Creating threads by using the Thread constructor

--- src/main/java/com/huerta/isra/threadmanipulation/App.java
+++ src/main/java/com/huerta/isra/threadmanipulation/App.java
@@ -3,20 +3,25 @@ package com.huerta.isra.threadmanipulation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class Runner1 {
+class Runner1 implements Runnable {
   private static final Logger log = LoggerFactory.getLogger(Runner1.class);
 
-  public void execute() {
+  @Override
+  public void run() {
     for (int i = 0; i < 10; i++) {
       log.info("Runner1: {}", i);
     }
   }
 }
 
-class Runner2 {
+class Runner2 implements Runnable {
   private static final Logger log = LoggerFactory.getLogger(Runner2.class);
 
   public void execute() {
+  }
+
+  @Override
+  public void run() {
     for (int i = 0; i < 10; i++) {
       log.info("Runner2: {}", i);
     }
@@ -28,11 +33,11 @@ public class App {
   private static final Logger log = LoggerFactory.getLogger(App.class);
 
   public static void main(String[] args) {
-    var ruuner1 = new Runner1();
-    var runner2 = new Runner2();
+    var t1 = new Thread(new Runner1());
+    var t2 = new Thread(new Runner2());
 
-    ruuner1.execute();
-    runner2.execute();
+    t1.start();
+    t2.start();
   }
 
 }
